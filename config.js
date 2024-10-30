a8b8f856d5d96b6fc71d8eac3933e8cfcd10e8b3
santipaez <sr.paez@alumno.um.edu.ar>
2024-10-30 06:33:38 -0300

    URL de eventos

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 export const AUTH_URL = 'http://192.168.100.30:8080/auth';
 export const SCHEDULE_URL = 'http://192.168.100.30:8080/schedule';
+export const EVENT_URL = 'http://192.168.100.30:8080/event';
